bd144c8ee3d7fce8ceb6f038dceda7944ed76407
fish-boy76 <141286018+fish-boy76@users.noreply.github.com>
2023-12-28 12:18:24 -0500

    Change Header to a Search Bar and New Background Add the first 15 dishes as an array of objects in the new script.js file. Add two new functions to script.js, populateRecipes() and recipeSearch(). Neither of the above functions are actually written yet. Other minor style changes.

---
+++ script.js
@@ -0,0 +1,115 @@
+const recipes = [
+    {
+        id: "1",
+        name: "Mushroom Skewer",
+        ingredients: ["Any Mushroom"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "2",
+        name: "Meat and Mushroom Skewer",
+        ingredients: ["Any Mushroom", "Any Meat"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "3",
+        name: "Fish and Mushroom Skewer",
+        ingredients: ["Any Mushroom", "Any Fish"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "4",
+        name: "Meat Skewer",
+        ingredients: ["Any Meat"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "5",
+        name: "Fish Skewer",
+        ingredients: ["Any Fish"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "6",
+        name: "Seafood Skewer",
+        ingredients: ["Any Snail or Crab"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "7",
+        name: "Copious Meat Skewers",
+        ingredients: ["Any Four Different Meat"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "8",
+        name: "Copious Seafood Skewers",
+        ingredients: ["Any Four Different Fish"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "9",
+        name: "Steamed Fruit",
+        ingredients: ["Any Fruit", "Any Vegetable"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "10",
+        name: "Steamed Tomatoes",
+        ingredients: ["Hylian Tomato", "Any Vegetable"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "11",
+        name: "Steamed Mushrooms",
+        ingredients: ["Any Mushroom", "Any Vegetable"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "12",
+        name: "Steamed Meat",
+        ingredients: ["Any Meat", "Any Vegetable"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "13",
+        name: "Steamed Fish",
+        ingredients: ["Any Fish", "Any Vegetable"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "14",
+        name: "Sauteed Peppers",
+        ingredients: ["Spicy Pepper"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    },
+    {
+        id: "15",
+        name: "Sauteed Nuts",
+        ingredients: ["Any Nut"],
+        effect: "Effect Not Available",
+        method: "Cooked In Pot"
+    }
+]
+
+function populateRecipes () {
+    
+}
+
+function recipeSearch() {
+    
+}
